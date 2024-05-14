e43f6ab6dc3c284f23b310ca93bebcc054fef59c
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-05-14 20:13:48 +0545

    Initial

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and couldn't process your request", http.StatusInternalServerError)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gyaneshwar01/greenlight
+
+go 1.22.2
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
