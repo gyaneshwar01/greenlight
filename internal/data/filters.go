5028f004a3a229042aefb061faf62d75b5c86d1c
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-05-21 17:23:43 +0545

    pagination with metadata

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,6 +1,11 @@
 package data
 
-import "github.com/gyaneshwar01/greenlight/internal/validator"
+import (
+	"math"
+	"strings"
+
+	"github.com/gyaneshwar01/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
@@ -16,4 +21,54 @@ func ValidateFilters(v *validator.Validator, f Filters) {
 	v.Check(f.PageSize <= 100, "page_size", "must be a maximum of 100")
 
 	v.Check(validator.In(f.Sort, f.SortSafelist...), "sort", "invalid sort value")
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
+}
+
+func (f Filters) limit() int {
+	// number of records to return
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	// number of rows to skip
+	return (f.Page - 1) * f.PageSize
+}
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page"`
+	PageSize     int `json:"page_size"`
+	FirstPage    int `json:"first_page"`
+	LastPage     int `json:"last_page"`
+	TotalRecords int `json:"total_records"`
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage:  page,
+		PageSize:     pageSize,
+		FirstPage:    1,
+		LastPage:     int(math.Ceil(float64(totalRecords) / float64(pageSize))),
+		TotalRecords: totalRecords,
+	}
 }
