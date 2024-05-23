dc62e95a69492ecc8f58f0528d77aa10f436595f
gyaneshwar01 <gyaneshwar585@gmail.com>
2024-05-23 13:33:14 +0545

    user registration

--- internal/data/models.go
+++ internal/data/models.go
@@ -4,10 +4,12 @@ import "database/sql"
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
